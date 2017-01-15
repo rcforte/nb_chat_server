281c23a77e23db66d6691f34a87271c8ddb6fe79
rcforte <rcforte@gmail.com>
2017-01-15 12:28:59 -0500

    Upgrading pom dependencies

--- nb_chat_server/src/test/java/chat/server/ChatTest.java
+++ nb_chat_server/src/test/java/chat/server/ChatTest.java
@@ -15,78 +15,77 @@ import org.junit.Test;
 import chat.server.ChatServer;
 
 public class ChatTest {
-	private ExecutorService m_executor = Executors.newFixedThreadPool(2);
-	private ChatServer m_server;
-	private BlockingChatClient m_client;
-	private FakeChatListener m_clientListener;
+    private ExecutorService m_executor = Executors.newFixedThreadPool(2);
+    private ChatServer m_server;
+    private BlockingChatClient m_client;
+    private FakeChatListener m_clientListener;
 
-	@Before
-	public void setUp() throws Exception, InterruptedException {
-		startServer();
-		startClient();
-	}
+    @Before
+    public void setUp() throws Exception, InterruptedException {
+        startServer();
+        startClient();
+    }
 
-	@After
-	public void tearDown() {
-		m_client.stop();
-		m_server.stop();
-	}
-	
-	private void startClient() {
-		m_clientListener = new FakeChatListener();
-		m_client = new BlockingChatClient();
-		m_client.addListener(m_clientListener);
-		m_executor.execute(new Runnable() {
-			@Override
-			public void run() {
-				m_client.connect();
-			}
-		});
-		m_client.waitForResult();
-	}
+    @After
+    public void tearDown() {
+        m_client.stop();
+        m_server.stop();
+    }
 
-	private void startServer() 
-		throws Exception, InterruptedException 
-	{
-		m_server = new ChatServer(9999);
-		m_executor.execute(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					m_server.start();
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		TimeUnit.SECONDS.sleep(1);
-	}
+    private void startClient() {
+        m_clientListener = new FakeChatListener();
+        m_client = new BlockingChatClient();
+        m_client.addListener(m_clientListener);
+        m_executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                m_client.connect();
+            }
+        });
+        m_client.waitForResult();
+    }
 
-	@Test
-	public void shouldConnect() {
-		assertEquals(FakeChatListener.ON_CONNECTED, 
-			m_clientListener.getResult());
-	}
+    private void startServer()
+            throws Exception, InterruptedException {
+        m_server = new ChatServer(9999);
+        m_executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    m_server.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        TimeUnit.SECONDS.sleep(1);
+    }
 
-	@Test
-	public void shouldFailConnectWhenServerIsStopped() {
-		m_client.stop();
-		m_server.stop();
-		try {
-			TimeUnit.SECONDS.sleep(1);
-		} catch(InterruptedException e) {
-			e.printStackTrace();
-		}
-		startClient();
-		assertEquals(FakeChatListener.ON_FAIL, 
-			m_clientListener.getResult());
-	}
-	
-	@Test
-	public void shouldGetRooms() {
-		m_client.getChatRooms();
-		m_client.waitForResult();
-		assertEquals(Arrays.asList("room1", "room2"), 
-			m_clientListener.getResults());
-	}
+    @Test
+    public void shouldConnect() {
+        assertEquals(FakeChatListener.ON_CONNECTED,
+                m_clientListener.getResult());
+    }
+
+    @Test
+    public void shouldFailConnectWhenServerIsStopped() {
+        m_client.stop();
+        m_server.stop();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startClient();
+        assertEquals(FakeChatListener.ON_FAIL,
+                m_clientListener.getResult());
+    }
+
+    @Test
+    public void shouldGetRooms() {
+        m_client.getChatRooms();
+        m_client.waitForResult();
+        assertEquals(Arrays.asList("room1", "room2"),
+                m_clientListener.getResults());
+    }
 }
